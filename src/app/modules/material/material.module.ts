13f8cf94fdc3fa8ecd5507632f9249965037f5be
soroushby <49848393+soroushby@users.noreply.github.com>
2020-06-30 04:06:02 -0700

    updated

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -3,10 +3,28 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
 })
 export class MaterialModule {}
